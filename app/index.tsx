f790c6e8ce54ff1869f7defba432226f134b48cb
lomadhavan <lomadhavan@ebay.com>
2024-06-10 13:18:44 -0400

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>
+        Test
+      </Text>
+    </View>
+  );
+}
